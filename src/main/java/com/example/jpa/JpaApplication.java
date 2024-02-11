6f8c4280c0f0f5f5fed19650ba790aba7d721265
V3NOM <rishabhrawat1977@gmail.com>
2024-02-11 19:21:11 +0530

    Pagination & Sorting

--- src/main/java/com/example/jpa/JpaApplication.java
+++ src/main/java/com/example/jpa/JpaApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class JpaApplication {
@@ -17,6 +20,11 @@ public class JpaApplication {
 		return args -> {
 			for (var i = 0; i < 5; i++)
 				repo.save(new User("User " + (i + 1)));
+			Sort sort = Sort.by(
+					Sort.Order.asc("name"),
+					Sort.Order.desc("id"));
+			Pageable pageable = PageRequest.of(0, 5, sort);
+			System.out.println(repo.findAll(pageable).getContent());
 		};
 	}
 
