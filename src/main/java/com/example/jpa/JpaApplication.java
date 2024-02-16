f6cb7c6693ffe9a883559976a499370d978615c3
V3NOM <rishabhrawat1977@gmail.com>
2024-02-17 00:44:40 +0530

    nothing

--- src/main/java/com/example/jpa/JpaApplication.java
+++ src/main/java/com/example/jpa/JpaApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+// import org.springframework.data.domain.PageRequest;
+// import org.springframework.data.domain.Pageable;
+// import org.springframework.data.domain.Sort;
 
-import com.example.jpa.models.User;
+// import com.example.jpa.models.Address;
+// import com.example.jpa.models.User;
 import com.example.jpa.repositories.UserRepo;
 import com.example.jpa.repositories.UserRepoWithoutJpaRepo;
 
@@ -22,15 +23,15 @@ public class JpaApplication {
 	@Bean
 	CommandLineRunner runner(UserRepo repo, UserRepoWithoutJpaRepo repoWithoutJpaRepo) {
 		return args -> {
-			for (var i = 0; i < 5; i++)
-				repo.save(new User("User " + (i + 1)));
-			Sort sort = Sort.by(
-					Sort.Order.asc("userName"),
-					Sort.Order.desc("id"));
-			Pageable pageable = PageRequest.of(0, 5, sort);
-			System.out.println(repo.findAll(pageable).getContent());
-			System.out.println("-----------------------------------------");
-			System.out.println(repoWithoutJpaRepo.findAll());
+			// for (var i = 0; i < 5; i++)
+			// 	repo.save(new User("User " + (i + 1), new Address("121004")));
+			// Sort sort = Sort.by(
+			// 		Sort.Order.asc("userName"),
+			// 		Sort.Order.desc("id"));
+			// Pageable pageable = PageRequest.of(0, 5, sort);
+			// System.out.println(repo.findAll(pageable).getContent());
+			// System.out.println("-----------------------------------------");
+			// System.out.println(repoWithoutJpaRepo.findAll());
 		};
 	}
 
